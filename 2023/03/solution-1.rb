0ce6568f20cac123a6c57bab5a94328fc3d565e1
Murray Steele <muz@h-lame.com>
2023-12-03 09:31:24 +0000

    2023 Day 3 - part 1
    Probably there is a more performant way to build this, but breaking the problem down into lots of little problems and "modelling" something is just how my brain works.  Lets see if part 2 forces the issue.

---
+++ 2023/03/solution-1.rb
@@ -0,0 +1,72 @@
+class Solution
+  class Normalizer
+    def self.do_it(file_name)
+      File.readlines(file_name).map &:chomp
+    end
+  end
+
+  def initialize(schematic)
+    @schematic = schematic
+  end
+
+  def result
+    parts.sum { |x| x.to_i }
+  end
+
+  def symbols
+    return @symbols if defined? @symbols
+
+    @symbols = []
+    schematic.each.with_index do |rows, y|
+      rows.each_char.with_index do |cell, x|
+        @symbols << [cell, [x, y]] unless cell.match? /\.|\d/
+      end
+    end
+    @symbols
+  end
+
+  def numbers
+    return @numbers if defined? @numbers
+
+    @numbers = []
+    cur_num = nil
+    schematic.each.with_index do |rows, y|
+      rows.each_char.with_index do |cell, x|
+        if cur_num
+          if cell.match? /\d/
+            cur_num[0] += cell
+            cur_num[-1][0] = x
+          else
+            cur_num = nil
+          end
+        else
+          if cell.match? /\d/
+            cur_num = [cell, [x, y], [x, y]]
+            @numbers << cur_num
+          end
+        end
+      end
+    end
+    @numbers
+  end
+
+  def parts
+    numbers.select do |n|
+      number, start, finish = *n
+      range = [start[0]-1..finish[0]+1, start[1]-1..finish[1]+1]
+      symbols.detect do |s|
+        _symbol, (x, y) = *s
+        range[0].cover?(x) && range[1].cover?(y)
+      end
+    end.map { |n| n.first }
+  end
+
+  private
+
+  attr_reader :schematic
+
+end
+
+if __FILE__ == $0
+  puts Solution.new(Solution::Normalizer.do_it(ARGV[0])).result
+end
---
+++ 2023/03/spec-1.rb
@@ -0,0 +1,121 @@
+require File.join(__dir__, 'solution-1')
+
+RSpec.describe Solution do
+  let(:raw_schematic) { [
+    '467..114..',
+    '...*......',
+    '..35..633.',
+    '......#...',
+    '617*......',
+    '.....+.58.',
+    '..592.....',
+    '......755.',
+    '...$.*....',
+    '.664.598..',
+  ] }
+
+  describe Solution::Normalizer do
+    it 'converts a file into an array of strings, one per line' do
+      expect(described_class.do_it File.join(__dir__,'example.txt')).to eq raw_schematic
+    end
+  end
+
+  it 'extracts symbol positions from the schematic' do
+    expect(described_class.new(raw_schematic).symbols).to eq [
+      ['*', [3, 1]],
+      ['#', [6, 3]],
+      ['*', [3, 4]],
+      ['+', [5, 5]],
+      ['$', [3, 8]],
+      ['*', [5, 8]],
+    ]
+  end
+
+  it 'extracts number positions from the schematic' do
+    expect(described_class.new(raw_schematic).numbers).to eq [
+      ['467', [0, 0], [2, 0]],
+      ['114', [5, 0], [7, 0]],
+      ['35', [2, 2], [3, 2]],
+      ['633', [6, 2], [8, 2]],
+      ['617', [0, 4], [2, 4]],
+      ['58', [7, 5], [8, 5]],
+      ['592', [2, 6], [4, 6]],
+      ['755', [6, 7], [8, 7]],
+      ['664', [1, 9], [3, 9]],
+      ['598', [5, 9], [7, 9]],
+    ]
+  end
+
+  it 'does not extract a part if there is no symbol' do
+    expect(
+      described_class.new([
+        '...',
+        '.1.',
+        '...'
+      ]).parts
+    ).to eq []
+  end
+
+  it 'extracts a part if there is a symbol adjecent to it' do
+    expect(
+      described_class.new([
+        '...',
+        '.1.',
+        '..*'
+      ]).parts
+    ).to eq ['1']
+    expect(
+      described_class.new([
+        '...',
+        '.1.',
+        '.*.'
+      ]).parts
+    ).to eq ['1']
+    expect(
+      described_class.new([
+        '...',
+        '.1.',
+        '*..'
+      ]).parts
+    ).to eq ['1']
+    expect(
+      described_class.new([
+        '...',
+        '*1.',
+        '...'
+      ]).parts
+    ).to eq ['1']
+    expect(
+      described_class.new([
+        '...',
+        '.1*',
+        '...'
+      ]).parts
+    ).to eq ['1']
+    expect(
+      described_class.new([
+        '..*',
+        '.1.',
+        '...'
+      ]).parts
+    ).to eq ['1']
+    expect(
+      described_class.new([
+        '.*.',
+        '.1.',
+        '...'
+      ]).parts
+    ).to eq ['1']
+    expect(
+      described_class.new([
+        '*..',
+        '.1.',
+        '...'
+      ]).parts
+    ).to eq ['1']
+  end
+
+  it 'gives the correct solution for the example' do
+    expect(described_class.new(raw_schematic).result).to eq 4361
+  end
+end
