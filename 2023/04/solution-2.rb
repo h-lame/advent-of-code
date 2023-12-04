e2215554c69de9e94a6ed2b93d2c6ab21379949a
Murray Steele <muz@h-lame.com>
2023-12-04 09:04:14 +0000

    2023 Day 4 part 2 - first pass, too slow
    At least in ruby this simplistic approach is too slow (e.g. on the input I haven't seen it complete).  I suspect the problem is that `to_see` is scaling hugely and I'm just eating too much memory with lots of repeated effort.  Let's see what we can do to speed this up.

---
+++ 2023/04/solution-2.rb
@@ -0,0 +1,67 @@
+class Solution2
+  class Normalizer
+    def self.do_it(file_name)
+      File.readlines(file_name).map &:chomp
+    end
+  end
+
+  Card = Data.define(:id, :winning_numbers, :numbers) do
+    def self.generate_from(card_string)
+      id_str, numbers_str = *card_string.split(':')
+      id = id_str.match(/Card\s+(\d+)/)[1].to_i
+      winning_numbers_str, numbers_str = numbers_str.split('|')
+      winning_numbers = winning_numbers_str.scan(/\d+/).map(&:to_i)
+      numbers = numbers_str.scan(/\d+/).map(&:to_i)
+      new(id:, winning_numbers:, numbers:)
+    end
+
+    attr_reader :matches
+
+    def initialize(id:, winning_numbers:, numbers:)
+      @matches = (winning_numbers & numbers).size
+      super
+    end
+
+    def score
+      if [0,1,2].include? matches
+        matches
+      else
+        2 ** (matches-1)
+      end
+    end
+
+    def prizes
+      (id+1).upto(id+matches).to_a
+    end
+  end
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def result
+    to_see = prepared_cards.map &:id
+    total = 0
+    while to_see.any?
+      card_id = to_see.shift
+      total += 1
+      to_see += prepared_cards[card_id-1].prizes
+    end
+    total
+  end
+
+  def prepared_cards
+    return @prepared_cards if defined? @prepared_cards
+
+    @prepared_cards = cards.map { |card| Card.generate_from(card) }
+  end
+
+  private
+
+  attr_reader :cards
+
+end
+
+if __FILE__ == $0
+  puts Solution2.new(Solution2::Normalizer.do_it(ARGV[0])).result
+end
---
+++ 2023/04/spec-2.rb
@@ -0,0 +1,49 @@
+require File.join(__dir__, 'solution-2')
+
+RSpec.describe Solution2 do
+  let(:raw_scratchcards) { [
+    'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+    'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+    'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+    'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+    'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+    'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11',
+  ] }
+
+  describe Solution2::Normalizer do
+    it 'converts a file into an array of strings, one per line' do
+      expect(described_class.do_it File.join(__dir__,'example.txt')).to eq raw_scratchcards
+    end
+  end
+
+  describe Solution2::Card do
+    it 'extracts an id, a list of winning numbers, and a list of numbers on the card from a string' do
+      card = described_class.generate_from raw_scratchcards[0]
+      expect(card.id).to eq 1
+      expect(card.winning_numbers).to eq [41, 48, 83, 86, 17]
+      expect(card.numbers).to eq [83, 86, 6, 31, 17, 9, 48, 53]
+    end
+
+    it 'calculates a score based on 2* the number of numbers in the winning set and the card set' do
+      expect(described_class.generate_from(raw_scratchcards[0]).score).to eq 8
+      expect(described_class.generate_from(raw_scratchcards[1]).score).to eq 2
+      expect(described_class.generate_from(raw_scratchcards[2]).score).to eq 2
+      expect(described_class.generate_from(raw_scratchcards[3]).score).to eq 1
+      expect(described_class.generate_from(raw_scratchcards[4]).score).to eq 0
+      expect(described_class.generate_from(raw_scratchcards[5]).score).to eq 0
+    end
+
+    it 'tells us the ids of the prize cards' do
+      expect(described_class.generate_from(raw_scratchcards[0]).prizes).to eq [2, 3, 4, 5]
+      expect(described_class.generate_from(raw_scratchcards[1]).prizes).to eq [3, 4]
+      expect(described_class.generate_from(raw_scratchcards[2]).prizes).to eq [4, 5]
+      expect(described_class.generate_from(raw_scratchcards[3]).prizes).to eq [5]
+      expect(described_class.generate_from(raw_scratchcards[4]).prizes).to eq []
+      expect(described_class.generate_from(raw_scratchcards[5]).prizes).to eq []
+    end
+  end
+
+  it 'gives the correct solution for the example' do
+    expect(described_class.new(raw_scratchcards).result).to eq 30
+  end
+end
