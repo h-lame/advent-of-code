0c117ea887b6d845a9643305e6cb20ff9b8df2d6
Murray Steele <muz@h-lame.com>
2023-12-09 23:05:15 +0000

    2023 Day 9 Part 1
    I don't know if part 2 will be significantly harder (although I have my suspicious - some big numbers in that input.txt).  Committing part 1 because it was my first attempt to actually look at the input.txt data rather than immediately scream when my solution gave me the wrong answer.  turns out, I hadn't accounted for negative numbers in the data file when converting, so I was getting `6` instead of `-6` as the first number of the last reading in the input.txt file.  This meant my solution didn't work and gave the wrong answer.  Would have been nice, I suspect, to have some errors if my assumptions were incorrect (e.g. the final row wasn't all 0s).

---
+++ 2023/09/solution-1.rb
@@ -0,0 +1,50 @@
+class Solution1
+  class Normalizer
+    def self.do_it(file_name)
+      File.readlines(file_name).map(&:chomp).map { |x| x.scan(/-?\d+/).map(&:to_i) }
+    end
+  end
+
+  History = Data.define(:readings) do
+    def next_reading
+      next_rows = extrapolate_rows
+      readings[-1] + next_rows[0][-1]
+    end
+
+    def extrapolate_rows
+      rows = []
+      row = readings
+      while !(row.all? &:zero?)
+        rows << row = row.each_cons(2).map { |x| x[1] - x[0] }
+      end
+      rows.reverse.map.with_index do |row, idx|
+        if idx.zero?
+          row << 0
+        else
+          prev_row = rows[0-idx]
+          row << row[-1] + prev_row[-1]
+        end
+        row
+      end.reverse
+    end
+  end
+
+  def initialize(readings)
+    @raw_readings = readings
+  end
+
+  def result
+    prepared_readings.sum { |r| r.next_reading }
+  end
+
+  private
+
+  attr_reader :raw_readings
+
+  def prepared_readings = raw_readings.map { |reading| History.new reading }
+
+end
+
+if __FILE__ == $0
+  puts Solution1.new(Solution1::Normalizer.do_it(ARGV[0])).result
+end
---
+++ 2023/09/spec-1.rb
@@ -0,0 +1,49 @@
+require File.join(__dir__, 'solution-1')
+
+RSpec.describe Solution1 do
+  let(:raw_readings) { [
+    [0, 3, 6, 9, 12, 15],
+    [1, 3, 6, 10, 15, 21],
+    [10, 13, 16, 21, 30, 45],
+  ] }
+
+  describe described_class::Normalizer do
+    it 'converts a file into an array of arrays of numbers, one per line' do
+      expect(described_class.do_it File.join(__dir__,'example.txt')).to eq raw_readings
+    end
+
+    it 'converts a file into an array of arrays of numbers (including negatives), one per line' do
+      expect(described_class.do_it File.join(__dir__,'example-with-negatives.txt')).to eq [[-6, -3, 8, 40]]
+    end
+  end
+
+  describe described_class::History do
+    it 'calculates the correct next value' do
+      expect(described_class.new(raw_readings[0]).next_reading).to eq 18
+      expect(described_class.new(raw_readings[1]).next_reading).to eq 28
+      expect(described_class.new(raw_readings[2]).next_reading).to eq 68
+    end
+
+    it 'calculates the next rows' do
+      expect(described_class.new(raw_readings[0]).extrapolate_rows).to eq [
+        [3, 3, 3, 3, 3, 3],
+        [0, 0, 0, 0, 0],
+      ]
+      expect(described_class.new(raw_readings[1]).extrapolate_rows).to eq [
+        [2, 3, 4, 5, 6, 7],
+        [1, 1, 1, 1, 1],
+        [0, 0, 0, 0],
+      ]
+      expect(described_class.new(raw_readings[2]).extrapolate_rows).to eq [
+        [3, 3, 5, 9, 15, 23],
+        [0, 2, 4, 6, 8],
+        [2, 2, 2, 2],
+        [0, 0, 0],
+      ]
+    end
+  end
+
+  it 'gives the correct solution for the examples' do
+    expect(described_class.new(raw_readings).result).to eq 114
+  end
+end
